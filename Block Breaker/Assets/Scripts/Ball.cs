eea153a940f5e6e0283258bc2ded8113d82283eb
Josephs <djosephs@LAMU02R820KG8WP.uhc.com>
2016-12-07 14:27:07 -0500

    Added brick breaker

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+	public Paddle paddle;
+	private bool hasStarted = false; 
+	private Vector3 paddleToBallVector;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!hasStarted) {
+			// lock the ball relative to the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			//Wait for a mouse press to launch
+			if (Input.GetMouseButtonDown (0)) {
+				print ("Mouse Clicked, Ball Launched");
+				this.rigidbody2D.velocity = new Vector2 (2f, 10f);
+				hasStarted = true;
+			}
+		}
+	}
+}
---
+++ Block Breaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log ("Level Load requested for " + name);
+		Application.LoadLevel (name);
+	}
+		
+
+	public void QuitRequest() {
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+}
